15203e4136eed9bac073c71d953a02b2567370cb
Viktoryia Hrushko <viktoryiahrushko@gmail.com>
2022-05-27 22:46:31 -0700

    second_2 commit TestNG

--- src/test/java/WebTest1_1.java
+++ src/test/java/WebTest1_1.java
@@ -41,14 +41,6 @@ public class WebTest1_1 {
     }
 
 
-    //TC_11_01 Подтвердите, что на странице по базовой ссылке в правом верхнем углу пользователь видит заголовок 99 Bottles of Beer
-    //
-    //Шаги:
-    //1. Открыть вебсайт на базовой странице
-    //2. Считать заголовок в правом верхнем углу
-    //3. Подтвердить, что текст заголовка соответствует ожидаемому
-    //4. Закрыть браузер
-
     @Test
     public void testTitleOfThePage(){
         String chromeDriver = "webdriver.chrome.driver";
@@ -72,13 +64,7 @@ public class WebTest1_1 {
 
     }
 
-    //TC_11_02 Подтвердите, что на странице по базовой ссылке последний пункт меню называется Submit new Language
-    //
-    //Шаги:
-    //1. Открыть вебсайт на базовой странице
-    //2. Считать название последнего пункта меню
-    //3. Подтвердить, что название последнего пункта меню соответствует ожидаемому
-    //4. Закрыть браузер
+
     @Test
 
     public void testMenuSubmitNewLanguage(){
@@ -101,14 +87,7 @@ public class WebTest1_1 {
         driver.quit();
     }
 
-    //TC_11_03 Подтвердите, что на странице по базовой ссылке последний пункт меню имеет подзаголовок Submit new Language
-    //
-    //Шаги:
-    //1. Открыть вебсайт на базовой странице
-    //2. Нажать на пункт меню Submit new Language
-    //3. Считать название подзаголовка последнего пункта меню
-    //4. Подтвердить, что название подзаголовка последнего пункта меню соответствует ожидаемому
-    //5. Закрыть браузер
+
 
     @Test
     public void testTitleOfSubmitNewLanguage(){
@@ -140,13 +119,7 @@ public class WebTest1_1 {
 
     }
 
-    //TC_11_04 Подтвердите, что на странице по ссылке http://www.99-bottles-of-beer.net/abc.html , первый пункт подменю называется 0-9
-    //
-    //Шаги:
-    //1. Открыть вебсайт на странице
-    //2. Считать название первого подзаголовка
-    //3. Подтвердить, что название подменю соответствует ожидаемому
-    //4. Закрыть браузер
+
     @Test
     public void testABCPage(){
         String chromeDriver = "webdriver.chrome.driver";
@@ -166,10 +139,7 @@ public class WebTest1_1 {
         driver.quit();
     }
 
-    //TC_11_06 Подтвердите, что имена создателей сайта:
-    //Oliver Schade
-    //Gregor Scheithauer
-    //Stefan Scheler
+
     @Test
     public void testNames(){
         String chromeDriver = "webdriver.chrome.driver";
@@ -209,16 +179,7 @@ public class WebTest1_1 {
         driver.quit();
 
     }
-    //TC_11_11 Подтвердите, что если на странице по ссылке http://www.99-bottles-of-beer.net/submitnewlanguage.html ,  пользователь нажмет кнопку Submit Language,  не заполнив информацию в обязательных полях, будет показана ошибка
-    //
-    //Error: Precondition failed - Incomplete Input.
-    //
-    //Шаги:
-    //1. Открыть вебсайт на странице
-    //2. Нажать на кнопку Submit Language
-    //3. Подтвердить, что на странице показана ошибка
-    //4. Подтвердить, что текст ошибки соответствует ожидаемому
-    //5. Закрыть браузер
+
     @Test
     public void testError(){
         String chromeDriver = "webdriver.chrome.driver";
